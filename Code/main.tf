ea3a54e61b01d8cdee16521c07c7eb1d0defc45b
Nikhil <nikhilraval1510@gmail.com>
2026-05-22 10:47:08 +0200

    feat: complete master CI/CD pipeline with Terraform automation

--- Code/main.tf
+++ Code/main.tf
@@ -6,6 +6,19 @@ resource "aws_instance" "automated_server" {
   ami           = "ami-04e601abe3e1a910f" 
   instance_type = "t3.micro"
 
+# THE DEVOPS BRIDGE: this runs automatically on first boot
+user_data = <<-EOF
+	#!/bin/bash
+	# 1. Update the server and install Docker
+	sudo apt-get update -y
+	sudo apt-get install docker.io -y
+	sudo systemctl start docker
+	sudo systemctl enable docker
+
+	#2. Pull your secure image from Docker Hub and run it!
+	sudo docker run -d -p 80:8080 guchuu2115/my-first-app:lastest
+	EOF
+
   tags = {
     Name = "Automated-Terraform-Server"
   }
