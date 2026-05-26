b7297e9647ad5f70bf19cffa36952f176a7900d9
Nikhil <nikhilraval1510@gmail.com>
2026-05-26 11:13:29 +0200

    feat: add environmnet variables for AWS RDS database

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 const http = require('http');
 const { Client } = require('pg');
 
-// 1. Configure the connection to match our docker-compose.yml
+// 1. Configure connection to read Environment Variables injected by Terraform!
 const client = new Client({
-  user: 'admin',
-  host: 'database', // Docker automatically routes this to the Postgres container!
-  database: 'enterprise_data',
-  password: 'secretpassword',
+  user: process.env.DB_USER || 'admin',
+  password: process.env.DB_PASSWORD || 'secretpassword',
+  database: process.env.DB_NAME || 'enterprise_data',
+  host: process.env.DB_HOST || 'database', // This will now catch the AWS RDS URL!
   port: 5432,
 });
 
 // 2. Connect to the database
 client.connect()
   .then(() => console.log('✅ Connected to PostgreSQL!'))
-  .catch(err => { 
+  .catch(err => {
     console.error('❌ Database connection error', err.stack);
-    process.exit(1); // DEVOP FIX: Force crash so Docker restarts it!
-});
+    process.exit(1); // Force crash so Docker restarts it!
+  });
 
 // 3. Start the Web Server
 const server = http.createServer(async (req, res) => {
   try {
-    // Ask the database what time it is to prove the connection works
     const dbRes = await client.query('SELECT NOW()');
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(`Container Live! Database time is: ${dbRes.rows[0].now}\n`);
+    res.end(`Production Live! AWS RDS time is: ${dbRes.rows[0].now}\n`);
   } catch (err) {
     res.writeHead(500, { 'Content-Type': 'text/plain' });
     res.end('Database disconnected.\n');
@@ -34,4 +33,3 @@ const server = http.createServer(async (req, res) => {
 server.listen(8080, () => {
   console.log('Server running on port 8080');
 });
-//terminal reset once again
